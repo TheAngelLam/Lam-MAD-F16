efd3bcc912a2d8f5d90896c91013dfdef4e42baf
angel <angellam@rgnt2-97-181-dhcp.int.colorado.edu>
2016-12-08 12:01:46 -0700

    changing images-working

--- Android Final-Ice Cream/FinalIceCream/app/src/main/java/com/example/angellam/finalicecream/FindIceCreamActivity.java
+++ Android Final-Ice Cream/FinalIceCream/app/src/main/java/com/example/angellam/finalicecream/FindIceCreamActivity.java
@@ -3,6 +3,7 @@ package com.example.angellam.finalicecream;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -19,6 +20,25 @@ public class FindIceCreamActivity extends AppCompatActivity {
         Spinner iceCreamSpinner = (Spinner) findViewById(R.id.spinner);
         String iceCreamValue = String.valueOf(iceCreamSpinner.getSelectedItem());
 
+        //Image
+        ImageView iceCreamImage = (ImageView) findViewById(R.id.iceCreamImageView);
+
+        //set the image based on spinner selection
+        switch (iceCreamValue){
+            case "Death by Chocolate":
+                iceCreamImage.setImageResource(R.drawable.chocolate);
+                break;
+            case "Salted Caramel":
+                iceCreamImage.setImageResource(R.drawable.caramel);
+                break;
+            case "Cookies and Cream":
+                iceCreamImage.setImageResource(R.drawable.cookiescream);
+                break;
+            default:
+                break;
+        }
+
+        //Result TextView
         TextView iceCreamResult = (TextView) findViewById(R.id.iceCreamResultTextView);
         iceCreamResult.setText("You really like " + iceCreamValue + " ice cream!");
     }
