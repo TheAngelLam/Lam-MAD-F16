d2b7a2a52f3912d25adc763c5fcedd4c36a64052
angel <angellam@rgnt2-97-181-dhcp.int.colorado.edu>
2016-12-08 14:09:00 -0700

    second activity working

--- Android Final-Ice Cream/FinalIceCream-second activity working/app/src/main/java/com/example/angellam/finalicecream/ReceiveIceCreamActivity.java
+++ Android Final-Ice Cream/FinalIceCream-second activity working/app/src/main/java/com/example/angellam/finalicecream/ReceiveIceCreamActivity.java
@@ -1,11 +1,8 @@
 package com.example.angellam.finalicecream;
 
 import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.os.Bundle;
 import android.widget.TextView;
 
 public class ReceiveIceCreamActivity extends AppCompatActivity {
@@ -28,23 +25,5 @@ public class ReceiveIceCreamActivity extends AppCompatActivity {
         //update TextView
         TextView messageView = (TextView) findViewById(R.id.iceCreamShopTextView);
         messageView.setText("You should check out " + iceCreamShop);
-
-        //get button
-        final Button button = (Button) findViewById(R.id.websiteButton);
-        //create listener
-        View.OnClickListener onclick = new View.OnClickListener(){
-            public void onClick(View view){
-                loadWebsite(view);
-            }
-        };
-
-        //add listener to button
-        button.setOnClickListener(onclick);
-    }
-
-    public void loadWebsite (View view){
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(Uri.parse(iceCreamShopURL));
-        startActivity(intent);
     }
 }
