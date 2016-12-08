ec4e36b1e489e927196e01a59776adc2429958b5
angel <angellam@rgnt2-97-181-dhcp.int.colorado.edu>
2016-12-08 12:27:20 -0700

    toggle working

--- Android Final-Ice Cream/FinalIceCream/app/src/main/java/com/example/angellam/finalicecream/FindIceCreamActivity.java
+++ Android Final-Ice Cream/FinalIceCream/app/src/main/java/com/example/angellam/finalicecream/FindIceCreamActivity.java
@@ -3,9 +3,12 @@ package com.example.angellam.finalicecream;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class FindIceCreamActivity extends AppCompatActivity {
 
@@ -16,9 +19,33 @@ public class FindIceCreamActivity extends AppCompatActivity {
     }
 
     public void onTreatMeButtonPressed(View view) {
+        //editText
+        EditText nameTreat = (EditText) findViewById(R.id.nameTreatEditText);
+        String nameValue = nameTreat.getText().toString();
+        System.out.println(nameValue);
+
+        //switch
+        String dairyfreeString = "";
+        Switch dairyfreeSwitch = (Switch) findViewById(R.id.dairyfreeSwitch);
+        boolean dairyfree = dairyfreeSwitch.isChecked();
+        if(dairyfree){
+            dairyfreeString = " that is dairy free.";
+        }
+
         //spinner
         Spinner iceCreamSpinner = (Spinner) findViewById(R.id.spinner);
         String iceCreamValue = String.valueOf(iceCreamSpinner.getSelectedItem());
+
+        //toggle button
+        ToggleButton toggle = (ToggleButton) findViewById(R.id.cupScoopToggleButton);
+        boolean container = toggle.isChecked();
+        String containerString;
+        if(container){
+            containerString = "cup";
+        } else {
+            containerString = "scoop";
+        }
+
 
         //Image
         ImageView iceCreamImage = (ImageView) findViewById(R.id.iceCreamImageView);
@@ -40,6 +67,6 @@ public class FindIceCreamActivity extends AppCompatActivity {
 
         //Result TextView
         TextView iceCreamResult = (TextView) findViewById(R.id.iceCreamResultTextView);
-        iceCreamResult.setText("You really like " + iceCreamValue + " ice cream!");
+        iceCreamResult.setText("My " + nameValue + " is a " + iceCreamValue + " ice cream " + containerString + dairyfreeString);
     }
 }
